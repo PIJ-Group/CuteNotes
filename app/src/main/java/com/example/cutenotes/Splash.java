4e3158083c7f57fb1ac720eed3a8f70341d9e842
Isra <pirravk@gmail.com>
2023-01-26 14:17:57 +0100

    Creamos proyecto con Splash animaciones y recursos

---
+++ app/src/main/java/com/example/cutenotes/Splash.java
@@ -0,0 +1,52 @@
+package com.example.cutenotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity implements Animation.AnimationListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        /*TextView text = findViewById(R.id.loading);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.splash);
+        text.startAnimation(animation);*/
+        TextView splash_cu = findViewById(R.id.splash_cu);
+        TextView splash_te = findViewById(R.id.splash_te);
+        TextView splash_tasks = findViewById(R.id.splash_tasks);
+
+        Animation cu_animation = AnimationUtils.loadAnimation(this,R.anim.cu_anim);
+        Animation te_animation = AnimationUtils.loadAnimation(this,R.anim.te_anim);
+        Animation tasks_animation = AnimationUtils.loadAnimation(this,R.anim.tasks_anim);
+
+        splash_cu.startAnimation(cu_animation);
+        splash_te.startAnimation(te_animation);
+        splash_tasks.startAnimation(tasks_animation);
+        tasks_animation.setAnimationListener(this);
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        Intent intent = new Intent(Splash.this,Login.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
