1ad559ea43a26b5adfa81dfc0bc28d9893513022
Pablo Baldazo <akibal1960@gmail.com>
2023-01-29 12:41:09 +0100

    Modificamos temas e iconos

--- app/src/main/java/com/example/cutenotes/MainActivity.java
+++ app/src/main/java/com/example/cutenotes/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         db = FirebaseFirestore.getInstance();
         nAuth = FirebaseAuth.getInstance();
         emailUser = nAuth.getCurrentUser().getEmail();
